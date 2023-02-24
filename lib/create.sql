7955200c78280a36d464d1026ee88bedb3d844bd
prince475 <danielmwai475@gmail.com>
2023-02-24 04:11:29 +0300

    Completed Assignment on SQL querries and basic commands

---
+++ lib/create.sql
@@ -0,0 +1,2 @@
+CREATE TABLE IF NOT EXISTS bears(id INTEGER PRIMARY KEY AUTOINCREMENT, name Text, age INTEGER, sex TEXT, color TEXT, temperament TEXT, alive BOOLEAN)
+
---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, sex) VALUES
+('Mr.Chocolate', 'M'),
+('Rowdy', 'M'),
+('Tabitha','F'),
+('Sergeant Brown', 'M'),
+('Mellisa', 'F'),
+('Grinch', 'M'),
+('Wendy', 'F'),
+(NULL,NULL);
